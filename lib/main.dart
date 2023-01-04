be47e1efad5df0990f005df5696eaed2e207f37d
kangsudal_notebook <kangsudal@gmail.com>
2023-01-04 20:36:08 +0900

    Neumorphic Calculator 완성

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 const primaryColor = Color(0xFF151026);
 void main() {
@@ -222,17 +223,70 @@ class NeumorphicCalculatorApp extends StatelessWidget {
           Expanded(
             flex: 4,
             child: Container(
-              color: Color(0xfff2f6f6),
+              color: Color(0xffdcdbdb),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Container(
+                        decoration: const BoxDecoration(
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.black38,
+                              blurRadius: 10.0,
+                              spreadRadius: -5.0,
+                              offset: Offset(5.0, 8.0),
+                            )
+                          ],
+                        ),
+                        child: Image.asset('images/calculator1.png')),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Container(
+                        decoration: const BoxDecoration(
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.black38,
+                              blurRadius: 10.0,
+                              spreadRadius: -5.0,
+                              offset: Offset(5.0, 8.0),
+                            )
+                          ],
+                        ),
+                        child: Image.asset('images/calculator2.png')),
+                  ),
+                ],
+              ),
             ),
           ),
           Expanded(
             flex: 1,
             child: Container(
               color: Colors.white,
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                child: Row(
+                  children: [
+                    Expanded(child: Text('Neumorphic Calculator',style: TextStyle(fontWeight: FontWeight.bold))),
+                    IconButton(onPressed: (){_launchUrl('https://calculator-neumorphic-aa62d.firebaseapp.com/');}, icon: Icon(Icons.play_arrow)),
+                    IconButton(onPressed: (){_launchUrl('https://kangsudal.tistory.com/entry/%EA%B3%84%EC%82%B0%EA%B8%B0%ED%99%98%EC%9C%A8%EA%B3%84%EC%82%B0%EA%B8%B0');}, icon: Icon(Icons.sticky_note_2_outlined)),
+                    IconButton(onPressed: (){_launchUrl('https://github.com/kangsudal/calculator_neumorphic');}, icon: Image.asset('icons/github-mark.png')),
+
+                  ],
+                ),
+              ),
             ),
           ),
         ],
       ),
     );
+  }
+}
+Future<void> _launchUrl(String url) async {
+  final Uri _url = Uri.parse(url);
+  if (!await launchUrl(_url)) {
+    throw 'Could not launch $_url';
   }
 }
