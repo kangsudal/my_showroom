96b0c47e03f12d7c8ac1c1cd3f14836655f9d86b
kangsudal_notebook <kangsudal@gmail.com>
2023-01-04 18:27:01 +0900

    레이아웃

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+const primaryColor = Color(0xFF151026);
 void main() {
   runApp(const MyApp());
 }
@@ -11,11 +12,226 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+        primaryColor: primaryColor,
+        appBarTheme: AppBarTheme(
+            iconTheme: IconThemeData(color: Color(0xff195b8e)),
+            color: Color(0xff195b8e)),
+      ),
       home: Scaffold(
-        body: CustomScrollView(
-          slivers: [
+        appBar: AppBar(
+          // title: Text('kangsudal-dev@naver.com'),
+          flexibleSpace: Padding(
+            padding: const EdgeInsets.only(left: 8.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  '강수경',
+                  style: TextStyle(color: Colors.white),
+                ),
+                Text(
+                  '01035880646',
+                  style: TextStyle(color: Colors.white),
+                ),
+                Text(
+                  'kangsudal-dev@naver.com',
+                  style: TextStyle(color: Colors.white),
+                )
+              ],
+            ),
+          ),
+        ),
+        body: ListView(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Whateatgo(),
+                NeumorphicCalculatorApp(),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                MillyApp(),
+                PerformanceTrackerApp(),
+              ],
+            ),
           ],
         ),
+      ),
+    );
+  }
+}
+
+class PerformanceTrackerApp extends StatelessWidget {
+  const PerformanceTrackerApp({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.all(40),
+      height: 400,
+      width: 400,
+      clipBehavior: Clip.hardEdge,
+      decoration: const BoxDecoration(
+        borderRadius: BorderRadius.all(Radius.circular(10)),
+        boxShadow: <BoxShadow>[
+          BoxShadow(
+            color: Colors.black38,
+            blurRadius: 10.0,
+            spreadRadius: -5.0,
+            offset: Offset(0.0, 10.0),
+          )
+        ],
+      ),
+      child: Column(
+        children: [
+          Expanded(
+            flex: 4,
+            child: Container(
+              color: Color(0xff4256bf),
+            ),
+          ),
+          Expanded(
+            flex: 1,
+            child: Container(
+              color: Colors.white,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class Whateatgo extends StatelessWidget {
+  const Whateatgo({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.all(40),
+      height: 400,
+      width: 400,
+      clipBehavior: Clip.hardEdge,
+      decoration: const BoxDecoration(
+        borderRadius: BorderRadius.all(Radius.circular(10)),
+        boxShadow: <BoxShadow>[
+          BoxShadow(
+            color: Colors.black38,
+            blurRadius: 10.0,
+            spreadRadius: -5.0,
+            offset: Offset(0.0, 10.0),
+          )
+        ],
+      ),
+      child: Column(
+        children: [
+          Expanded(
+            flex: 4,
+            child: Container(
+              color: Color(0xffd9deeb),
+            ),
+          ),
+          Expanded(
+            flex: 1,
+            child: Container(
+              color: Colors.white,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class MillyApp extends StatelessWidget {
+  const MillyApp({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.all(40),
+      height: 400,
+      width: 400,
+      clipBehavior: Clip.hardEdge,
+      decoration: const BoxDecoration(
+        borderRadius: BorderRadius.all(Radius.circular(10)),
+        boxShadow: <BoxShadow>[
+          BoxShadow(
+            color: Colors.black38,
+            blurRadius: 10.0,
+            spreadRadius: -5.0,
+            offset: Offset(0.0, 10.0),
+          )
+        ],
+      ),
+      child: Column(
+        children: [
+          Expanded(
+            flex: 4,
+            child: Container(
+              color: Color(0xffFBE960),
+            ),
+          ),
+          Expanded(
+            flex: 1,
+            child: Container(
+              color: Colors.white,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class NeumorphicCalculatorApp extends StatelessWidget {
+  const NeumorphicCalculatorApp({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.all(40),
+      height: 400,
+      width: 400,
+      clipBehavior: Clip.hardEdge,
+      decoration: const BoxDecoration(
+        borderRadius: BorderRadius.all(Radius.circular(10)),
+        boxShadow: <BoxShadow>[
+          BoxShadow(
+            color: Colors.black38,
+            blurRadius: 10.0,
+            spreadRadius: -5.0,
+            offset: Offset(0.0, 10.0),
+          )
+        ],
+      ),
+      child: Column(
+        children: [
+          Expanded(
+            flex: 4,
+            child: Container(
+              color: Color(0xfff2f6f6),
+            ),
+          ),
+          Expanded(
+            flex: 1,
+            child: Container(
+              color: Colors.white,
+            ),
+          ),
+        ],
       ),
     );
   }
